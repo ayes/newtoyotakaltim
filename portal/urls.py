618ffd09b9ad7e9820e504663fd89a4bcd6abd33
Irfan Mahfudz Guntur <ayes@bsmsite.com>
2014-03-21 05:33:22 +0800

    bsmchanges :smiley:

--- portal/urls.py
+++ portal/urls.py
@@ -2,11 +2,37 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
 	url(r'^product/?$', 'portal.views.product'),
-	url(r'^product/sedan/?', 'portal.views.product_sedan'),
-	url(r'^product/hatchback?', 'portal.views.product_hatchback'),
-	url(r'^product/mpv?', 'portal.views.product_mpv'),
-	url(r'^product/suv?', 'portal.views.product_suv'),
-	url(r'^product/commercial?', 'portal.views.product_commercial'),
-	url(r'^product/sport?', 'portal.views.product_sport'),
-	url(r'^product/hybrid?', 'portal.views.product_hybrid'),
+
+	url(r'^product/sedan/?$', 'portal.views.product_sedan'),
+	url(r'^product/sedan/camry/?', 'portal.views.product_sedan_camry'),
+	url(r'^product/sedan/all-new-vios/?', 'portal.views.product_sedan_all_new_vios'),
+	url(r'^product/sedan/all-new-corolla-altis/?', 'portal.views.product_sedan_all_new_corolla_altis'),
+
+	url(r'^product/hatchback/?$', 'portal.views.product_hatchback'),
+	url(r'^product/hatchback/yaris/?', 'portal.views.product_hatchback_yaris'),
+	url(r'^product/hatchback/agya/?', 'portal.views.product_hatchback_agya'),
+	url(r'^product/hatchback/etios-valco/?', 'portal.views.product_hatchback_etios_valco'),
+
+	url(r'^product/mpv/?$', 'portal.views.product_mpv'),
+	url(r'^product/mpv/alphard/?', 'portal.views.product_mpv_alphard'),
+	url(r'^product/mpv/nav1/?', 'portal.views.product_mpv_nav1'),
+	url(r'^product/mpv/kijang-innova/?', 'portal.views.product_mpv_kijang_innova'),
+
+	url(r'^product/suv/?$', 'portal.views.product_suv'),
+	url(r'^product/suv/land-cruiser/?', 'portal.views.product_land_cruiser'),
+	url(r'^product/suv/new-fortuner/?', 'portal.views.product_new_fortuner'),
+	url(r'^product/suv/new-rush/?', 'portal.views.product_new_rush'),
+
+	url(r'^product/commercial/?$', 'portal.views.product_commercial'),
+	url(r'^product/commercial/hiace/?', 'portal.views.product_hiace'),
+	url(r'^product/commercial/dyna/?', 'portal.views.product_dyna'),
+	url(r'^product/commercial/hilux-d-cab/?', 'portal.views.product_hilux_d_cab'),
+	url(r'^product/commercial/hilux-s-cab/?', 'portal.views.product_hilux_s_cab'),
+
+	url(r'^product/sport/?$', 'portal.views.product_sport'),
+	url(r'^product/sport/toyota-86/?', 'portal.views.product_toyota_86'),
+
+	url(r'^product/hybrid/?$', 'portal.views.product_hybrid'),
+	url(r'^product/hybrid/prius/?', 'portal.views.product_prius'),
+	url(r'^product/hybrid/camry-hybrid/?', 'portal.views.product_camry_hybrid'),
 )
--- portal/views.py
+++ portal/views.py
@@ -10,20 +10,77 @@ def product(request):
 def product_sedan(request):
 	return render_to_response('portal_product_sedan.html',{}, RequestContext(request))
 
+def product_sedan_camry(request):
+	return render_to_response('portal-product-sedan-camry.html',{}, RequestContext(request))
+
+def product_sedan_all_new_vios(request):
+	return render_to_response('portal-product-sedan-all-new-vios.html',{}, RequestContext(request))
+
+def product_sedan_all_new_corolla_altis(request):
+	return render_to_response('portal-product-sedan-all-new-corolla-altis.html',{}, RequestContext(request))
+
 def product_hatchback(request):
 	return render_to_response('portal_product_hatchback.html',{}, RequestContext(request))
+
+def product_hatchback_yaris(request):
+	return render_to_response('portal-product-hatchback-yaris.html',{}, RequestContext(request))
+
+def product_hatchback_agya(request):
+	return render_to_response('portal-product-hatchback-agya.html',{}, RequestContext(request))
+
+def product_hatchback_etios_valco(request):
+	return render_to_response('portal-product-hatchback-etios-valco.html',{}, RequestContext(request))
 
 def product_mpv(request):
 	return render_to_response('portal_product_mpv.html',{}, RequestContext(request))
 
+def product_mpv_alphard(request):
+	return render_to_response('portal-product-mpv-alphard.html',{}, RequestContext(request))
+
+def product_mpv_nav1(request):
+	return render_to_response('portal-product-mpv-nav1.html',{}, RequestContext(request))
+
+def product_mpv_kijang_innova(request):
+	return render_to_response('portal-product-mpv-kijang-innova.html',{}, RequestContext(request))
+
 def product_suv(request):
 	return render_to_response('portal_product_suv.html',{}, RequestContext(request))
+
+def product_suv_land_cruiser(request):
+	return render_to_response('portal_product_land_cruiser.html',{}, RequestContext(request))
+
+def product_suv_new_fortuner(request):
+	return render_to_response('portal_product_suv_new_fortuner.html',{}, RequestContext(request))
+
+def product_suv_new_rush(request):
+	return render_to_response('portal_product_new_rush.html',{}, RequestContext(request))
 
 def product_commercial(request):
 	return render_to_response('portal_product_commercial.html',{}, RequestContext(request))
 
+def product_commercial_hiace(request):
+	return render_to_response('portal_product_commercial_hiace.html',{}, RequestContext(request))
+
+def product_commercial_dyna(request):
+	return render_to_response('portal_product_commercial_dyna.html',{}, RequestContext(request))
+
+def product_commercial_hilux_d_cab(request):
+	return render_to_response('portal_product_commercial_hilux_d_cab.html',{}, RequestContext(request))
+
+def product_commercial_hilux_s_cab(request):
+	return render_to_response('portal_product_commercial_hilux_s_cab.html',{}, RequestContext(request))
+
 def product_sport(request):
 	return render_to_response('portal_product_sport.html',{}, RequestContext(request))
 
+def product_sport_toyota_86(request):
+	return render_to_response('portal_product_sport_toyota_86.html',{}, RequestContext(request))
+
 def product_hybrid(request):
 	return render_to_response('portal_product_hybrid.html',{}, RequestContext(request))
+
+def product_hybrid_prius(request):
+	return render_to_response('portal_product_hybrid_prius.html',{}, RequestContext(request))
+
+def product_hybrid_camry_hybrid(request):
+	return render_to_response('portal_product_hybrid_camry_hybrid.html',{}, RequestContext(request))
